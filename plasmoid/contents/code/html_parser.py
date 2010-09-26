0f83000255deb81b637d6a617a2eb32213a50178
Ahmet Emre Aladağ <aladagemre@gmail.com>
2010-09-26 19:18:39 +0300

    * Updated authors * Added context menu: copy to clipboard * Added Twitter & FriendFeed share links

--- plasmoid/contents/code/html_parser.py
+++ plasmoid/contents/code/html_parser.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010, Volkan Esgel, Ahmet Emre Aladağ
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
 from string import strip
 import random
 
@@ -32,14 +43,25 @@ class Line:
         self.text, self.author = line.split("|")
         self.merge()
 
+    def get_twitter_link(self):
+	return '<a href="http://twitter.com/home?status=%s" title="Click to share this on Twitter">Twitter</a>' % self.plain_text
+	
+    def get_ff_link(self):
+	return '<a href="http://friendfeed.com/share/bookmarklet/frame#title=%s" title="Click to share this on FriendFeed">FriendFeed</a>' % self.plain_text
+	
     def merge(self):
         """Merges text and author fields as two paragraphs."""
         # might need some quoting for " here.
-        self.merged_text = '<p>%s</p>' % self.text
+        self.plain_text = "%s" % self.text
+        self.html_text = '<p>%s</p>' % self.text
         if self.author:
-            self.merged_text += '<p align="right"><strong><i>%s</i></strong></p>' % self.author
+	    self.plain_text += " -- %s" % self.author
+            self.html_text+= '<p align="right"><strong><i>%s</i></strong></p>' % self.author
 
+	self.html_text += "<p>Share on %s</p>" % ( " / ".join( (self.get_twitter_link(), self.get_ff_link()) ) )
+	
     def __repr__(self):
-        return self.merged_text
+        return self.html_text
+        
     def __unicode__(self):
         return unicode(self.__repr__())
--- plasmoid/contents/code/main.py
+++ plasmoid/contents/code/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010, Volkan Esgel
+# Copyright (C) 2010, Volkan Esgel, Ahmet Emre Aladağ
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
@@ -15,7 +15,22 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
-from html_parser import *
+from html_parser import Document
+
+class LineDisplayer(Plasma.TextBrowser):
+    def __init__(self, parent=None):
+	Plasma.TextBrowser.__init__(self, parent)
+	self.parent = parent
+	
+    def contextMenuEvent(self, event):
+	self.menu = menu = QMenu(None)
+	copyToClipboard = menu.addAction("Copy to clipboard")
+	
+	action = menu.exec_(event.screenPos())
+        
+        if action == copyToClipboard:
+	    clipboard = QApplication.clipboard()
+	    clipboard.setText(self.line.plain_text)
 
 class RollingBoard(plasmascript.Applet):
 
@@ -37,14 +52,15 @@ class RollingBoard(plasmascript.Applet):
         self.mainLayout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
         self.document = Document(self.package().path() + "contents/code/words_of_wisdom.txt")
 
-        #text = "<b><i>Just a try</i></b>. This is an experimental plasmoid and may not work properly."
-        text = self.document.get_random_line().__unicode__()
+        self.line = self.document.get_random_line()
+        text = self.line.__unicode__()
 
-        textBrowser = Plasma.TextBrowser(self.applet)
+        textBrowser = LineDisplayer(self.applet)
+        textBrowser.line = self.line
         textBrowserNW = textBrowser.nativeWidget()
         textBrowserNW.setReadOnly(True)
         textBrowser.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        textBrowser.setText(text) # unicode(text, 'UTF-8'))
+        textBrowser.setText(text)
         textBrowser.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
         textBrowser.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)        
         self.mainLayout.addItem(textBrowser)
